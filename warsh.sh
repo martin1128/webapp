10687bdd1b1d733bb2e61e2b7dab7777961ee8b3
1102343666 <1102343666@qq.com>
2019-07-19 00:37:02 +0800

    333

--- warsh.sh
+++ warsh.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 chmod 777 /var/lib/jenkins/workspace/webapp/*.sh
 rm -f /var/lib/jenkins/workspace/webapp/warsh.sh
-/usr/app/java/jdk1.8.0_191/bin/java -jar -cvf webapp_new.war ./*
+/usr/app/java/jdk1.8.0_191/bin/java -jar -cvf /var/lib/jenkins/workspace/webapp/webapp_new.war ./*
 
 #echo "start success!"
 exit 0
