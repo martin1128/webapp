cd18d95100daeba47fccccd8a0d2858b44714001
1102343666 <1102343666@qq.com>
2019-07-18 23:32:47 +0800

    333

---
+++ warsh.sh
@@ -0,0 +1,23 @@
+@echo off
+
+TITLE Create TMS War
+
+cls
+echo ==================================
+echo ==                              ==
+echo ==  TITLE Create TMS War ==
+echo ==                              ==
+echo ==================================
+
+echo -------------------------------
+
+@rem …Ë÷√Java–Èƒ‚ª˙¬∑æ∂
+
+
+echo JAVA_HOME = %JAVA_HOME%
+
+"%JAVA_HOME%\bin\jar" cvf  webapp.war *
+
+pause
+
+@echo on
