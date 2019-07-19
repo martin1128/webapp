48e9db7be13bec6c61723fc4cd748dbc7b324444
dds <dds@qq.com>
2019-07-19 16:02:20 +0800

    2222

--- WebContent/webapp.sh
+++ WebContent/webapp.sh
@@ -1,6 +1,6 @@
 #!/bin/bash 
  
-/usr/wls/appsoft/jdk1.8.0_191/bin/java -jar -cvf /root/.jenkins/workspace/lis_dat/webapp_new.war ./*
+/usr/wls/appsoft/jdk1.8.0_191/bin/jar -cvf webapp_new.war ./*
 
 #echo "start success!"
 exit 0
